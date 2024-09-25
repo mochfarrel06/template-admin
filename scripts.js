e7461ecf757b618d8b3b7630e171cb18c982c51d
MochFarrel06 <mochfarrel2606@gmail.com>
2024-09-25 21:34:24 +0700

    modification

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sidebarToggle = document.querySelector(".navbar-toggler");
-    const sidebar = document.querySelector("#sidebar");
+document.querySelectorAll('.sidebar-item .sidebar-link').forEach(function (link) {
+    link.addEventListener('click', function () {
+        const icon = this.querySelector('.icon-link');
+        const submenu = document.querySelector(this.getAttribute('href'));
 
-    sidebarToggle.addEventListener("click", function () {
-        sidebar.classList.toggle("d-none");
+        submenu.addEventListener('show.bs.collapse', function () {
+            icon.classList.add('rotate-90'); // Tambahkan kelas saat dropdown dibuka
+        });
+
+        submenu.addEventListener('hide.bs.collapse', function () {
+            icon.classList.remove('rotate-90'); // Hapus kelas saat dropdown ditutup
+        });
     });
 });
